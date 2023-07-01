4112d108cb6a3f795a5b9d359a59b3c70652023c
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-07-01 19:04:24 +0530

    Adding stories section

---
+++ components/Feed.js
@@ -0,0 +1,14 @@
+import Stories from '../components/Stories'
+export default function Feed() {
+  return (
+    <main>
+    <section>
+        <Stories/>
+        {/* Posts */}
+    </section>
+    <section>
+
+    </section>
+    </main>
+  )
+}
