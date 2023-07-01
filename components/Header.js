8eeaf54ad8afaced82975c490c6a0a3509da903f
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-07-01 16:02:15 +0530

    Added SearchBar

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 function Header() {
   return (
-    <div>
-      {/* Left */}
-
       <div className='flex items-center justify-between max-w-6xl'>
         <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
           <Image
@@ -21,9 +19,14 @@ function Header() {
           className='object-contain'
           />
         </div>
+        <div className='relative mt-1'>
+          <div className='absolute top-2 left-2'>
+            <MagnifyingGlassIcon className='h-5 text-gray-500'/>
+          </div>
+          <input type='text' placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+        </div>
         <h1>Right side</h1>
       </div>
-    </div>
   )
 }
 
