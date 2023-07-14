4e90e5f69eb76f3ac9b8b95b7a714e2e727de511
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-07-14 22:46:56 +0530

    Add session profile

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,12 @@ import Image from "next/image";
 import React from "react";
 import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Header() {
+
+  const {data:session} = useSession();
+ 
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
@@ -36,16 +40,25 @@ export default function Header() {
         </div>
 
         {/* Right */}
-
-        <div className="flex space-x-4 items-center">
+        {
+          session ? (
+            <>
+            <div className="flex space-x-4 items-center">
           <HomeIcon className="hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
           <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
           <img
-            src="https://lh3.googleusercontent.com/a/AAcHTtdTQTn5wex-Zzu7F4dFw8HdhSh_mW72V2iMTl7_646JVGWO=s96-c-rg-br100"
+          onClick={signOut}
+            src={session.user.image}
             alt="user-image"
             className="h-10 rounded-full cursor-pointer"
           />
         </div>
+            </>
+          ):(
+            <button onClick={signIn}>Sign in</button>
+          )
+        }
+        
       </div>
     </div>
   );
