d5d0960014325b7b03a76305928d7838615c3d2e
Abhinav1190P <pandeysandeep1190@gmail.com>
2023-07-18 18:12:31 +0530

    Hide on Session and Routing logos

--- components/Header.js
+++ components/Header.js
@@ -5,13 +5,14 @@ import { HomeIcon } from "@heroicons/react/solid";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atom/modalAtom";
+import { useRouter } from "next/router";
 
 export default function Header() {
 
   const {data:session} = useSession();
   const [open,setOpen] = useRecoilState(modalState)
 
-
+  const router = useRouter();
 
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
@@ -22,6 +23,7 @@ export default function Header() {
             src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
             layout="fill"
             className="object-contain"
+            onClick={()=>router.push("/")}
           />
         </div>
         <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
@@ -29,6 +31,7 @@ export default function Header() {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
             layout="fill"
             className="object-contain"
+            onClick={()=>router.push("/")}
           />
         </div>
         {/* Middle */}
@@ -49,7 +52,9 @@ export default function Header() {
           session ? (
             <>
             <div className="flex space-x-4 items-center">
-          <HomeIcon className="hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
+          <HomeIcon 
+          onClick={()=>router.push("/")}
+          className="hidden md:inline-flex  h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
           <PlusCircleIcon 
           onClick={()=>setOpen(true)}
           className="h-6 cursor-pointer hover:scale-125 transition-tranform duration-200 ease-out" />
--- components/Posts.js
+++ components/Posts.js
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from 'react';
 import Post from './Post';
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
 import { db } from '../firebase'
+
+
 export default function Posts() {
 
     const [posts, setPosts] = useState([])
 
+
     useEffect(() => {
         const ubsubscribe = onSnapshot(
-            query(collection(db, 'posts'),orderBy(('timestamp'),"desc")), (snapshot) => {
-            setPosts(snapshot.docs)
-        })
+            query(collection(db, 'posts'), orderBy(('timestamp'), "desc")), (snapshot) => {
+                setPosts(snapshot.docs)
+            })
         return ubsubscribe
-    })
+    }, [db])
 
 
     return <div>
